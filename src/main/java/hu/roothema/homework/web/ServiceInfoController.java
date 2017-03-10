cb51d224b6bd7099ec6d1b8c3312730591995cf7
Máté Hársádi <mate.harsadi@gmail.com>
2017-03-11 00:07:02 +0100

    unused import

--- src/main/java/hu/roothema/homework/web/ServiceInfoController.java
+++ src/main/java/hu/roothema/homework/web/ServiceInfoController.java
@@ -1,6 +1,5 @@
 package hu.roothema.homework.web;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
