f1f47ac2e6d769db0e2aa28a301bcd3017163b36
Máté Hársádi <mate.harsadi@gmail.com>
2017-03-10 23:56:44 +0100

    js cleanup

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,24 +1,9 @@
 var stompClient = null;
 
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
-}
-
 function connect() {
     var socket = new SockJS('/action-monitor');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-
         stompClient.subscribe('/topic/action-message', function (message) {
             showMessage(JSON.parse(message.body));
         });
@@ -29,8 +14,6 @@ function disconnect() {
     if (stompClient != null) {
         stompClient.disconnect();
     }
-    setConnected(false);
-    console.log("Disconnected");
 }
 
 function showMessage(message) {
