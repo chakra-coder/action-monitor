efbc8d70096a377e9acc101ce2ca0c1840e93a10
Máté Hársádi <mate.harsadi@gmail.com>
2017-03-11 09:26:52 +0100

    Added activemq support & docker image building & docker-compose & readme update

---
+++ readme.md
@@ -0,0 +1,24 @@
+#Build and create docker image
+
+## Maven build
+    mvn clean install
+    
+## Build Docker image
+    mvn clean package docker:build -DskipTests
+    
+# Run the application via docker-compose
+    docker-compose up
+
+# Webpage access
+    Here you can see all the events related to the database changes. Right now only inserting is available.
+    
+    http://localhost:8080/
+    
+# Storing new 'Data'  
+   POST http://localhost:8080/v1/data
+   Headers:
+    Content-Type: application/json
+   Payload:
+       {
+        "data": "42"
+       }
--- src/main/java/hu/roothema/homework/WebSocketConfig.java
+++ src/main/java/hu/roothema/homework/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package hu.roothema.homework;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -10,9 +11,12 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+    @Value("${broker.host}")
+    private String host;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay("/topic").setRelayHost(host);
     }
 
     @Override
